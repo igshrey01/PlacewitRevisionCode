8d8bdd132580ebd7de4a8b9faf3c6fd68f73d575
Shrey Dangayach <54055303+igshrey01@users.noreply.github.com>
2023-07-01 11:36:07 +0530

    Create Combination Sum.cpp

---
+++ 01-07-2023/Class 1/Combination Sum.cpp
@@ -0,0 +1,38 @@
+//Code - https://leetcode.com/problems/combination-sum/
+
+class Solution {
+    
+    vector<vector<int>> res;
+    vector<int> tmp;
+public:
+    
+    void findCombination(vector<int>& candidates, int target, int in, int n){
+        
+        if(in == n){
+            
+            if(target == 0)
+                res.push_back(tmp);
+            
+            return;
+            
+        }
+        
+        //notPick
+        findCombination(candidates,target,in+1,n);
+        
+        tmp.push_back(candidates[in]);
+        if(target>=candidates[in])
+            findCombination(candidates,target-candidates[in],in,n);
+        
+        tmp.pop_back();
+        
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        
+        int n = candidates.size();
+        findCombination(candidates,target,0,n);
+        
+        return res;
+        
+    }
+};
